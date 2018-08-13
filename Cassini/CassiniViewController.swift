24b137a38385155b0680cff5d33c5ef7911430a0
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-08-13 16:38:19 +0800

    Segue

--- Cassini/CassiniViewController.swift
+++ Cassini/CassiniViewController.swift
@@ -9,5 +9,14 @@
 import UIKit
 
 class CassiniViewController: UIViewController {
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if let url = DemoURLs.NASA[identifier] {
+                if let imageViewController = segue.destination as? ImagegViewController {
+                    imageViewController.imageURL = url
+                    imageViewController.title = (sender as? UIButton)?.currentTitle
+                }
+            }
+        }
+    }
 }
