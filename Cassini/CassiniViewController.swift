2dbca63e061298fec8efbf7c7cdbffd5dfd89a6f
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-08-13 17:10:43 +0800

    Extension for contents for UIViewController

--- Cassini/CassiniViewController.swift
+++ Cassini/CassiniViewController.swift
@@ -12,15 +12,21 @@ class CassiniViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let identifier = segue.identifier {
             if let url = DemoURLs.NASA[identifier] {
-                var destination = segue.destination
-                if let navcontroller = destination as? UINavigationController {
-                    destination = navcontroller.visibleViewController ?? navcontroller
-                }
-                if let imageViewController = destination as? ImagegViewController {
+                if let imageViewController = segue.destination.contents as? ImagegViewController {
                     imageViewController.imageURL = url
                     imageViewController.title = (sender as? UIButton)?.currentTitle
                 }
             }
+        }
+    }
+}
+
+extension UIViewController {
+    var contents: UIViewController {
+        if let navCon = self as? UINavigationController {
+            return navCon.visibleViewController ?? self
+        } else {
+            return self
         }
     }
 }
