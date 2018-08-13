29e5719f4d2ab7255aa9c3e4e347f24836b527be
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-08-13 16:21:23 +0800

    Create split and navigation view

---
+++ Cassini/CassiniViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CassiniViewController.swift
+//  Cassini
+//
+//  Created by Svitlana Dzyuban on 13/8/18.
+//  Copyright © 2018 Lana Dzyuban. All rights reserved.
+//
+
+import UIKit
+
+class CassiniViewController: UIViewController {
+
+}
